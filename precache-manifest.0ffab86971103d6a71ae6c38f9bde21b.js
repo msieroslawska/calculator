fb618e59007bd0c5096904ab47160ecc99c6c0c8
Marta <msieroslawska@users.noreply.github.com>
2019-03-28 16:30:37 +0100

    Updates

---
+++ precache-manifest.0ffab86971103d6a71ae6c38f9bde21b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9915d74e384c267026d1",
+    "url": "/calculator/static/css/main.6741b293.chunk.css"
+  },
+  {
+    "revision": "9915d74e384c267026d1",
+    "url": "/calculator/static/js/main.9915d74e.chunk.js"
+  },
+  {
+    "revision": "a72738d72da637d2104a",
+    "url": "/calculator/static/js/1.a72738d7.chunk.js"
+  },
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "a612a729ef1aaf284e060e25c29134df",
+    "url": "/calculator/index.html"
+  }
+];
