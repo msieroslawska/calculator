b882200599d36082d4857107dc62b563f1bbaed3
Marta <msieroslawska@users.noreply.github.com>
2019-06-05 17:49:45 +0200

    Updates

--- precache-manifest.7e2373a5ecde512287fd781701f8b103.js
+++ precache-manifest.7e2373a5ecde512287fd781701f8b103.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d978aa86cc209528da787cf7772bde7",
+    "revision": "a2526cc386a72eaeac076d807067d7ae",
     "url": "/calculator/index.html"
   },
   {
-    "revision": "fa23aba8ec261442d899",
-    "url": "/calculator/static/css/main.75fb38bc.chunk.css"
+    "revision": "e77e5ca6fa2363088cc1",
+    "url": "/calculator/static/css/main.edb519b2.chunk.css"
   },
   {
     "revision": "64226e5183b875a2637e",
     "url": "/calculator/static/js/2.bd6dd1d8.chunk.js"
   },
   {
-    "revision": "fa23aba8ec261442d899",
-    "url": "/calculator/static/js/main.fdb25aef.chunk.js"
+    "revision": "e77e5ca6fa2363088cc1",
+    "url": "/calculator/static/js/main.0c099b54.chunk.js"
   },
   {
     "revision": "2fe8cbdbbea1fbf116e6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calculator/precache-manifest.7672b4b37f497459e42aca5a3e22b143.js"
+  "/calculator/precache-manifest.7e2373a5ecde512287fd781701f8b103.js"
 );
 
 self.addEventListener('message', (event) => {
